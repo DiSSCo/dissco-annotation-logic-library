4f55b73f640246afba63dd84afee12a4741df294
Soulaine <soulaine.theo@gmail.com>
2025-10-21 21:40:00 +0200

    initial commit

---
+++ src/main/java/eu/dissco/annotationlogic/AnnotationLogicApplication.java
@@ -0,0 +1,13 @@
+package eu.dissco.annotationlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationLogicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnnotationLogicApplication.class, args);
+  }
+
+}
---
+++ src/test/java/eu/dissco/annotationlogic/AnnotationLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dissco.annotationlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationLogicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
